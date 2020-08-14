a850ea178604c865d41c25ab34a47ed51cf2156c
nikigan <gankin.n@yandex.ru>
2020-08-14 12:31:16 +0700

    Отрефакторил сервис

--- src/services/serviceBase.js
+++ src/services/serviceBase.js
@@ -9,4 +9,20 @@ const instance = axios.create({
   },
 });
 
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    const status = error.response ? error.response.status : 408;
+    if (status >= 500) {
+      window.console.error(error.toString());
+    }
+    if (status === 401) {
+      window.console.log("logout");
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default instance;
