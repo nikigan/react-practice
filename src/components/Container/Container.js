38eb6cdd4d9315261a62a8105183afeb0ec8b2e8
nikigan <gankin.n@yandex.ru>
2020-08-17 15:22:59 +0700

    Опять эти заглушки...

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,9 +1,17 @@
+import PropTypes from "prop-types";
 import React from "react";
 import "./Container.scss";
 
-// eslint-disable-next-line react/prop-types
 const Container = ({ children }) => {
   return <div className="container">{children}</div>;
 };
 
 export default Container;
+
+Container.propTypes = {
+  children: PropTypes.element,
+};
+
+Container.defaultProps = {
+  children: null,
+};
